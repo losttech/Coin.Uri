3b224b1eef7a77181d1da124965417545c7cd2b5
Victor Milovanov <lost@losttech.software>
2019-11-07 20:23:28 -0800

    stub for checking coin status

--- coin.ts
+++ coin.ts
@@ -1,4 +1,4 @@
-function main(){
+async function main(){
     if (!window.location.hash || window.location.hash == '#') {
         return;
     }
@@ -13,12 +13,12 @@ function main(){
         console.error("only ETH is supported");
         return;
     }
-    let url = new URL("http://eth/?" + hash);
+    let url:any = new URL("http://eth/?" + hash);
     var query = "?";
     url.searchParams.forEach((val, param) => {
         if (query.length > 1) { query += "&"; }
         switch(param) {
-        case "tx":
+        case "release_tx":
         case "key":
             query += param + "=" + encodeURIComponent(val);
             break;
@@ -28,9 +28,30 @@ function main(){
         }
     });
 
-    var destination = "coin:eth" + query;
-    console.log(destination);
-    window.location.assign(destination);
+    let coinAddress = "..";
+    let web3: any = {
+        getBalance: async function() { return 10; },
+        getLastTransaction: async function() { return { timestamp: "2 days ago" }; },
+    };
+    let balance = await web3.getBalance(coinAddress);
+    if (balance > 0) {
+        var destination = "coin:eth" + query;
+        console.log(destination);
+
+        let claim = <HTMLAnchorElement>document.getElementById("claim");
+        claim.href = destination;
+
+        document.getElementById("claimed").remove();
+
+        window.location.assign(destination);
+    } else {
+        let lastTransaction = await web3.getLastTransaction();
+        document.getElementById("claimedDate").innerText = lastTransaction.timestamp;
+
+        document.getElementById("available").remove();
+    }
+
+    document.getElementById("loading").remove();
 }
 
 main();
